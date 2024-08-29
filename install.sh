8166c108a87db4b77c93051cf113559a1e61525d
MrKeith99 <kvalentincardenas@hotmail.com>
2024-08-29 12:01:03 +0900

    [FIX] installer

--- install.sh
+++ install.sh
@@ -1,31 +1,36 @@
 #!/bin/bash
 
-
 echo "╔══╣ Install: Speech Recognition Whisper (STARTING) ╠══╗"
 
+
+# Keep the current directory
+DIR=$(pwd)
+
 sudo apt-get update
+sudo apt-get install -y \
+    ffmpeg \
+    python3-pyaudio
 
-pip3 install whisper
+python3 -m pip install -U pip
+python3 -m pip install \
+    playsound
+# python3 -m pip install whisper
+python3 -m pip install git+https://github.com/openai/whisper.git 
 
-pip3 install git+https://github.com/openai/whisper.git
 
-sudo apt-get install ffmpeg
-
-sudo apt-get install python3-pyaudio
-
-pip3 install playsound
-
-cd ~/colcon_ws/src/speech_recognition_whisper/
+cd $DIR
 python3 install.py
 
 # Install "sobits_msgs"
-cd ~/colcon_ws/src/
-# git clone https://github.com/TeamSOBITS/sobits_msgs.git
-# cd ~/colcon_ws/src/speech_recognition_vosk/
+cd ..
+git clone https://github.com/TeamSOBITS/sobits_msgs.git
 
 # Install "alsamixer"
-sudo apt-get remove --purge alsa-base pulseaudio
-sudo apt-get install alsa-base pulseaudio
-sudo alsa force-reload
+# sudo apt-get remove --purge alsa-base pulseaudio
+# sudo apt-get install alsa-base pulseaudio
+# sudo alsa force-reload
+
+cd $DIR
+
 
 echo "╚══╣ Install: Speech Recognition Whisper (FINISHED) ╠══╝"
