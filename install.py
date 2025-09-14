76a3b27f19041e6cc7d5ba02293c3aca9f0e6067
Lucasmotabr <153493682+Lucasmotabr@users.noreply.github.com>
2025-09-15 00:18:42 +0900

    [ADD] Faster-Whisper install.py

--- install.py
+++ install.py
@@ -1,4 +1,15 @@
 import whisper
-model = whisper.load_model("small")
+from faster_whisper import WhisperModel
 
-# tiny, base, small, medium, large, large-v2, large-v3, large-v3-turbo
+# Pre-download OpenAI Whisper model
+whisper_model = whisper.load_model("small")
+
+# Pre-download Faster-Whisper model
+faster_model = WhisperModel("small", device="cuda", compute_type="float16")
+
+
+# Available Whisper models:
+# tiny, base, small, medium, large,large-v1, large-v2, large-v3, large-v3-turbo
+
+# Available Faster-Whisper models (as of now):
+# tiny, base, small, medium, large,large-v1, large-v2, large-v3
